19723d71b04c64fef48dd5fa34c2160a6177b54f
Jakob Zmudzinski <140855933+JakobZ919@users.noreply.github.com>
2024-03-14 22:19:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-
+ var a = [];
+    var op = "";
+    function addto(x){
+        document.getElementById("a1").innerText += String(x);
+    }
+    function opr(x){
+        a.push(Number(document.getElementById("a1").innerText));
+        document.getElementById("a1").innerText = " ";
+        op = "";
+        op = x;
+    }
+    function equal(){
+        a.push(Number(document.getElementById("a1").innerText));
+        if (op == "+"){
+            document.getElementById("a1").innerText = String(a[0] + a[1]);
+            a = [];
+        }
+        if (op == "-"){
+            document.getElementById("a1").innerText = String(a[0] - a[1]);
+            a = [];
+        }
+        if (op == "x"){
+            document.getElementById("a1").innerText = String(a[0] * a[1]);
+            a = [];
+        }
+        if (op == ":"){
+            document.getElementById("a1").innerText = String(a[0] / a[1]);
+            a = [];
+        }
+    }
